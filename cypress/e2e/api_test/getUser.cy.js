cc6a5da07470ca60ba1b7076c4c7d5955b863c6b
dksoni27 <dksoni27062001@gmail.com>
2025-06-22 15:13:04 +0530

    comment add new

--- cypress/e2e/api_test/getUser.cy.js
+++ cypress/e2e/api_test/getUser.cy.js
@@ -6,7 +6,7 @@ describe("get users by api", () => {
             method: 'GET',
             url: 'https://gorest.co.in/public/v2/users',
             header: {
-                'Authorization': 'Bearer '+tocken 
+                'Authorization': 'Bearer ' + tocken
             }
         }).then((res) => {
             expect(res.status).to.equal(200);
@@ -19,22 +19,26 @@ describe("get users by api", () => {
                 expect(user.email).to.be.a('string');
                 expect(user.gender).to.be.oneOf(['male', 'female']);
                 expect(user.status).to.be.oneOf(['active', 'inactive']);
+
+                const users = res.body;
+                const firstUserId = users[0].id;
+
+
+                cy.log('First user ID:', firstUserId);
+
+                cy.request({
+                    method: 'GET',
+                    url: 'https://gorest.co.in/public/v2/users/' + firstUserId,
+                    header: {
+                        'Authorization': 'Bearer ' + tocken
+                    }
+                }).then((res) => {
+                    expect(res.status).to.equal(200);
+                    const user1 = res.body;
+                    const name = user1.name
+                    cy.log(name);
+                })
             })
-
-        })
-    })
-
-    it('get Users by id', () => {
-        cy.request({
-            method: 'GET',
-            url: 'https://gorest.co.in/public/v2/users/7962136',
-            header: {
-                 'Authorization': 'Bearer '+tocken 
-            }
-        }).then((res) => {
-            expect(res.status).to.equal(200);
-            expect(res.body.name).to.equal('Sharda Panicker VM')
-        
 
         })
     })
